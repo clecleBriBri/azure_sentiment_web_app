34fb548831897c382a7325029b7118e9d38b6fff
Brigliano Clément <clement.brigliano@he-arc.ch>
2023-05-06 12:58:31 +0200

    added db

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ from azure.core.credentials import AzureKeyCredential
 from azure.ai.textanalytics import TextAnalyticsClient
 import os
 import requests
+from db import DBSentiment
 
 
 def authenticate_client():
-    key = "fbc517233178440e9c97da8d49fbb87a"
+    key = os.environ.get("AZURE_TEXT_ANALYTICS_KEY")
     endpoint = "https://cognitive-jc-review.cognitiveservices.azure.com/"
     credential = AzureKeyCredential(key)
     client = TextAnalyticsClient(endpoint=endpoint, credential=credential)
@@ -28,7 +29,10 @@ def sentiment_analysis(client, documents):
 
 
 def getReviews(name: str):
-    url = "https://fct-app-jc-review.azurewebsites.net/api/GetComments?code=N8Ttd1qwvB3iEaMhw0eJkkSf4XLmKlikMVPa3nazF5jJAzFuPesVVQ==&name=m3gan"
+    url = (
+        "https://fct-app-jc-review.azurewebsites.net/api/GetComments?code=N8Ttd1qwvB3iEaMhw0eJkkSf4XLmKlikMVPa3nazF5jJAzFuPesVVQ==&name="
+        + name
+    )
     headers = {"Content-type": "application/json"}
     response = requests.post(url, headers=headers)
     return response.text.replace("[", "").replace("]", "")
@@ -44,12 +48,27 @@ def index():
 
 @app.route("/", methods=["POST"])
 def analyze():
-    client = authenticate_client()
+    db_conn = DBSentiment()
     text = request.form["text"]
+    text = text.strip().replace(" ", "_").lower()
+    if len(db_conn.get_movie(text)) > 0:
+        movie_stats_from_db = db_conn.get_movie(text)[0]
+        return render_template(
+            "index.html",
+            movie_name=movie_stats_from_db[0],
+            partial_results=movie_stats_from_db[1],
+        )
+    client = authenticate_client()
+    if text == "":
+        return render_template("index.html", error="Please enter a name.")
+
     reviews = getReviews(text)
+    if len(reviews) == 0:
+        return render_template("index.html", error="No reviews found.")
     # https://stackoverflow.com/questions/952914/how-do-i-make-a-flat-list-out-of-a-list-of-lists
-    results = sentiment_analysis(client, reviews)
-    return render_template("index.html", results=results)
+    results = sentiment_analysis(client, [reviews])
+    db_conn.add_to_db(text, results[0]["sentiment"])
+    return render_template("index.html", results=results[0])
 
 
 if __name__ == "__main__":
--- db.py
+++ db.py
@@ -1,39 +1,78 @@
 import psycopg2
+import os
+
+
 class DBSentiment:
-    def init(self, sentiment: str, score: float):
-        self.conn = psycopg2.connect(
+    def connect(self):
+        return psycopg2.connect(
             host="c.cluster-jc-review.postgres.database.azure.com",
             database="citus",
             user="citus",
-            password="pass123!"
+            password=os.environ.get("DB_PASSWORD"),
         )
 
-    def delete_by_name(self, name: str):
-        cur = self.conn.cursor()
-
-        cur.execute("DELETE FROM sentiments WHERE sentiment = %s", (name))
-
-        self.conn.commit()
-
-        cur.close()
-        self.conn.close()
-
-    def add_to_db(self, name: str, sentiment: str):
-        cur = self.conn.cursor()
-
-        cur.execute("INSERT INTO sentiments VALUES (%s, %s)", (name, sentiment))
-
-        self.conn.commit()
-
-        cur.close()
-        self.conn.close()
-
-    def getAll(self):
-        cur = self.conn.cursor()
+    def show_all(self):
+        conn = self.connect()
+        cur = conn.cursor()
 
         cur.execute("SELECT * FROM sentiments")
 
         rows = cur.fetchall()
 
         cur.close()
-        self.conn.close()
+        conn.close()
+
+        return rows
+
+    def get_column_names(self):
+        conn = self.connect()
+        cur = conn.cursor()
+
+        cur.execute(
+            "SELECT column_name FROM information_schema.columns WHERE table_name = 'sentiments'"
+        )
+
+        rows = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return rows
+
+    def delete_by_name(self, name: str):
+        conn = self.connect()
+        cur = conn.cursor()
+
+        cur.execute("DELETE FROM sentiments WHERE movie = %s", (name,))
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+    def add_to_db(self, name: str, sentiment: str):
+        conn = self.connect()
+        cur = conn.cursor()
+
+        cur.execute(
+            "INSERT INTO sentiments (movie, sentiment) VALUES (%s, %s)",
+            (name, sentiment),
+        )
+
+        conn.commit()
+
+        cur.close()
+        conn.close()
+
+    def get_movie(self, name):
+        conn = self.connect()
+        cur = conn.cursor()
+
+        cur.execute("SELECT * FROM sentiments WHERE movie = %s", (name,))
+
+        rows = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return rows
